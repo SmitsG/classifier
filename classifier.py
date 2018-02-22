018afe942e31937a9ac2750ac1faf028aaf94496
SmitsG <gjt.smits@student.han.nl>
2018-02-22 16:42:49 +0100

    csv data handler added

--- classifier.py
+++ classifier.py
@@ -5,51 +5,74 @@ Spyder Editor
 This is a temporary script file.
 """
 import re
+import csv
 
 def main():
     csv_file = File()
-    filename = "C:/Users/Beheerder/Documents/GitHub/classifier/input.csv"
-    obj_valuelist = csv_file.get_values(filename)
-    
-        
+    path = "C:/Users/gerwi/PycharmProjects/classifier/input.csv"
+    data_list = csv_file.get_csv_values(path)
+
+
+# Class File does something with the file.
 class File:
-    
-    def get_values(self, filename):
-        start = False
-        with open(filename, "r") as csv_file:
-            obj_valuelist = []
-            for line in csv_file:
-                if re.match(r'^\d', line):
-                    start = True
-                if start == True:
-                     line = line.replace("\n", "")
-                     line = line.split(",")
-                     obj = Aminoacid(line[0], line[1], line[2])
-                     obj_valuelist.append(obj)
-        return(obj_valuelist)
-                     
-        
-class Aminoacid:
+
+    def get_csv_values(self, path):
+        """
+        :param path: filepath + filename
+        :return: data_list: List with data objects [obj1: polarity, hydrofobicity, given class, obj2 ...]
+        """
+        file = open(path, newline='')
+        reader = csv.reader(file)
+        data_list = []
+
+        header = next(reader)
+        data = []
+        for row in reader:
+            # row = [polarity, hydrofobicity, given_class]
+            polarity = int(row[0])
+            hydrofobicity = int(row[1])
+            given_class = int(row[2])
+            data = AminoAcid(polarity, hydrofobicity, given_class)
+            data_list.append(data)
+            data.get_hydrofobicity()
+        return data_list
+
+
+# Class AminoAcid stores amino_acid values.
+class AminoAcid:
     def __init__(self, polarity, hydrofobicity, given):
         self.pol = polarity
         self.hyd = hydrofobicity
         self.giv = given
         
     def get_polarity(self):
-        return(self.pol)
+        return self.pol
     
     def get_hydrofobicity(self):
-        return(self.hyd)
+        return self.hyd
     
-    def get_given(self):
-        return(self.giv)
+    def get_class_given(self):
+        return self.giv
     
-    def predicted(self):
-        return(self.pred)
+    def get_class_predicted(self):
+        return self.pred
     
-    def error(self):
-        return(self.error)
+    def get_error(self):
+        return self.error
         
 
-    
 main()
+
+# with open(path, "r") as csv_file:
+#     reader = csv.reader(csv_file)
+#     header = next(reader)
+#     for row in reader:
+#         print(row)
+#         if re.match(r'^\d', row):
+#             start = True
+#         if start == True:
+#              row = row.strip()
+#              row = row.split(",")
+#              obj = Amino_acid(row[0], row[1], row[2])
+#              obj_valuelist.append(obj)
+# return obj_valuelist
