b1ecad6dff3990fb9e45b41a318ccc5ebec773dc
SmitsG <smitsg@ziggo.nl>
2018-02-27 20:25:24 +0100

    add calculate_lowest_error

--- classifier.py
+++ classifier.py
@@ -6,6 +6,7 @@ This is a temporary script file.
 """
 from random import randint
 from CsvFile import CsvFile
+import matplotlib.pyplot as plt
 
 def main():
     csv_file = CsvFile()
@@ -14,7 +15,9 @@ def main():
     header = csv_file.skip_header(reader)
     data_dictionary = csv_file.get_csv_values(reader, csv_file)
     class_predicted_dictionary = calculate_predictions(data_dictionary)
-    calculate_error(data_dictionary, class_predicted_dictionary)
+    error_dictionary = calculate_error(data_dictionary, class_predicted_dictionary)
+    lowest_error_dictionary = calculate_lowest_error(error_dictionary)
+    make_plot(lowest_error_dictionary)
 
 
 def calculate_predictions(data_dictionary):
@@ -24,13 +27,19 @@ def calculate_predictions(data_dictionary):
     :return class_predicted_dictionary: key - row_name, value - predicted values for each row
     """
     class_predicted_dictionary = {}
-    for i in range(100):
+    for i in range(50):
         times = "times_" + str(i)
-        a, b = randomiser(0, 50) # Get a and b random values for Monte Carlo prediction.
+        a, b = randomiser(0, 10) # Get a and b random values for Monte Carlo prediction.
         for row, data in data_dictionary.items(): # Do this x times the range
             class_predicted_value = calculate_class_predicted(data[0], data[1], a, b)
             class_predicted_dictionary.setdefault(times, []).append(class_predicted_value) #adds multiple values without overwriting
     return class_predicted_dictionary
+
+
+def randomiser(lowest_value, highest_value):
+    a = randint(lowest_value, highest_value)
+    b = randint(lowest_value, highest_value)
+    return a, b
 
 
 def calculate_class_predicted(hydrofobicity, polarity, a, b):
@@ -38,21 +47,32 @@ def calculate_class_predicted(hydrofobicity, polarity, a, b):
     return class_predicted_value
 
 
-def calculate_error(data_dictionary, predicted_class_dictionary):
-    pass
-    # datarow = 0
-    # for times, predicted_class_list in predicted_class_dictionary.items():
-    #     for predicted_class in predicted_class_list:
-    #         datarow += 1
-    #         print(predicted_class, data_dictionary[])
-    #         error = data_dictionary["row_" + str(datarow)] - predicted_class
-    #         print(error)
-    #     datarow = 0
+def calculate_error(data_dictionary, class_predicted_dictionary):
+    error_dictionary = {}
+    count = 0
+    for times, predicted_class_list in class_predicted_dictionary.items():
+        for predicted_class in predicted_class_list: # voor 70
+            count += 1
+            list_with_given_class = data_dictionary.get("row_" + str(count))
+            error = ((list_with_given_class[2] - predicted_class) ** 2)
+            error_dictionary.setdefault(times, []).append(error)
+        count = 0
 
-def randomiser(lowest_value, highest_value):
-    a = randint(lowest_value, highest_value)
-    b = randint(lowest_value, highest_value)
-    return a, b
+    return error_dictionary
+
+
+def calculate_lowest_error(error_dictionary):
+    lowest_error_dictionary = {}
+    for times, error_list in error_dictionary.items():
+        lowest_error_dictionary[times] = min(error_list)
+    return lowest_error_dictionary
+
+
+def make_plot(lowest_error_dictionary):
+    x = 10
+    y = 20
+    plt.plot(x, y)
+
 
 
 main()
