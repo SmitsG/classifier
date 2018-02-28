8b474e308a5298609b07aa9725547184ad0380b2
SmitsG <smitsg@ziggo.nl>
2018-02-28 16:34:29 +0100

    added comments (input param, returns)

--- classifier.py
+++ classifier.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 from random import randint
 from CsvFile import CsvFile
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 def main():
     csv_file = CsvFile()
@@ -24,7 +24,7 @@ def calculate_predictions(data_dictionary):
     """
     Calculate class predicted for each row in dataset, x times.
     :param data_dictionary: key - row_name  , value - [hydrofobicity, polarity, class_given]
-    :return class_predicted_dictionary: key - row_name, value - predicted values for each row
+    :return class_predicted_dictionary: key - times (Monte Carlo), value - predicted values for each row in the csv file.
     """
     class_predicted_dictionary = {}
     for i in range(50):
@@ -32,22 +32,39 @@ def calculate_predictions(data_dictionary):
         a, b = randomiser(0, 10) # Get a and b random values for Monte Carlo prediction.
         for row, data in data_dictionary.items(): # Do this x times the range
             class_predicted_value = calculate_class_predicted(data[0], data[1], a, b)
-            class_predicted_dictionary.setdefault(times, []).append(class_predicted_value) #adds multiple values without overwriting
+            class_predicted_dictionary.setdefault(times, []).append(class_predicted_value) # adds multiple values without overwriting
     return class_predicted_dictionary
 
 
 def randomiser(lowest_value, highest_value):
+    """
+    :param lowest_value: lowest value for randomizing
+    :param highest_value: highest value for randomizing
+    :return: random value a, random value b
+    """
     a = randint(lowest_value, highest_value)
     b = randint(lowest_value, highest_value)
     return a, b
 
 
 def calculate_class_predicted(hydrofobicity, polarity, a, b):
+    """
+    :param hydrofobicity: Hydrofobicity of a certain row in the csv file.
+    :param polarity: Polarity of a certain row in the csv file.
+    :param a: random value a
+    :param b: random value b
+    :return: The class_predicted value.
+    """
     class_predicted_value = (a * hydrofobicity + b) - polarity
     return class_predicted_value
 
 
 def calculate_error(data_dictionary, class_predicted_dictionary):
+    """
+    :param data_dictionary: key - row_name  , value - [hydrofobicity, polarity, class_given]
+    :param class_predicted_dictionary: key - times (Monte Carlo), value - predicted values for each row in the csv file.
+    :return error_dictionary: key - times (Monte Carlo), value calculated error rate for each row of the csv file.
+    """
     error_dictionary = {}
     count = 0
     for times, predicted_class_list in class_predicted_dictionary.items():
@@ -57,11 +74,14 @@ def calculate_error(data_dictionary, class_predicted_dictionary):
             error = ((list_with_given_class[2] - predicted_class) ** 2)
             error_dictionary.setdefault(times, []).append(error)
         count = 0
-
     return error_dictionary
 
 
 def calculate_lowest_error(error_dictionary):
+    """
+    :param error_dictionary: key - times (Monte Carlo), value calculated error rate for each row of the csv file.
+    :return lowest_error_dictionary: key - times (Monte Carlo), value - lowest error calculated from all rows in the csv file.
+    """
     lowest_error_dictionary = {}
     for times, error_list in error_dictionary.items():
         lowest_error_dictionary[times] = min(error_list)
@@ -69,38 +89,11 @@ def calculate_lowest_error(error_dictionary):
 
 
 def make_plot(lowest_error_dictionary):
-    x = 10
-    y = 20
-    plt.plot(x, y)
-
+    pass
+    # print(lowest_error_dictionary)
+    # x = 10
+    # y = 20
+    # plt.plot(x, y)
 
 
 main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-#main
-# predictions_dictionary = Caluculations.calculate_predictions(data_list)
-
-#calculate
- # for data_object in data_list:
-            # class_predicted = calculate.calculate_class_predicted(AminoAcid.get_hydrofobicity(data_object),
-            #                                                       AminoAcid.get_polarity(data_object), a, b)
-#
-            # data_list = []
-            # data = AminoAcid(polarity, hydrofobicity, given_class)
-            # data_list.append(data)
-
-  # get class predicted for each object in the data_list.
-  #   and do this several amount of times (range).
-  #   store the values for each time in a dictionary.
